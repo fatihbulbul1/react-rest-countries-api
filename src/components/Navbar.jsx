dfd56b7433566b1e696b79cec7aa23abb767829c
fatihbulbul1 <76781482+fatihbulbul1@users.noreply.github.com>
2022-10-30 12:28:00 +0300

    bug fix for incompatible flags like nepal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = ({darkMode, setDarkMode}) => {
         <nav className={`${darkMode && 'dark-nav'}`}>
             <div className="nav-container">
                 <h1>Where in the world?</h1>
-                <button onClick={() => {
+                <button onClick={() => { 
                     if(!darkMode) document.querySelector('body').style.backgroundColor = 'hsl(207, 26%, 17%)';
                     else document.querySelector('body').style.backgroundColor = 'hsl(0, 0%, 98%)'; 
                     setDarkMode(!darkMode)
